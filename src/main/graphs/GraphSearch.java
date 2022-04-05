5be2dfbc1f27981843423f86e32b6eb25de09f04
JJ Brown <jack.brown.255@gmail.com>
2022-04-04 22:18:20 -0500

    Added some comments for readability and easier implementation

--- src/main/graphs/GraphSearch.java
+++ src/main/graphs/GraphSearch.java
@@ -2,6 +2,17 @@ package graphs;
 
 import java.util.List;
 
+/**
+ * An algorithm that can find the shortest path between two nodes in a graph.
+ */
 public interface GraphSearch {
+    /**
+     * Searches the graph and returns the shortest path from source to destination, if possible.
+     * May return {@code null} or an empty List if no such path exists.
+     * Should return a singleton List if the source and destination are the same.
+     *
+     * @param source the identifier for the node to start searching from; may be any integer, and might have no edges
+     * @param dest   the identifier for the node to search for from the source; may be any integer, and might have no edges
+     */
     List<Integer> findShortestPath(int source, int dest);
 }
--- src/main/graphs/ReachabilitySearch.java
+++ src/main/graphs/ReachabilitySearch.java
@@ -2,6 +2,15 @@ package graphs;
 
 import java.util.Set;
 
+/**
+ * An algorithm that can determine whether every node in a graph is reachable from a single source.
+ */
 public interface ReachabilitySearch {
+    /**
+     * Searches the graph and returns whether there is a path to every node in {@code allNodes} starting from {@code source}.
+     *
+     * @param source   the identifier for the node to start searching from; may be any integer, and might have no edges
+     * @param allNodes the set of all the vertices in this graph
+     */
     boolean canReachEveryNode(int source, Set<Integer> allNodes);
 }
